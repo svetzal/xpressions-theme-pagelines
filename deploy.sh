d84fc5dc918d30eb3cb1246ef9a970f70be36e22
Stacey Vetzal <stacey@vetzal.com>
2014-01-06 17:47:36 -0500

    strengthening deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 TARGET="root@commonsroot.com"
-LOCATION="/var/www/vhosts/xpressions.org/httpdocs/wp-content/themes/xpressions-theme"
-#LOCATION="/var/www/vhosts/xpressions.org/subdomains/new/httpdocs/wp-content/themes/xpressions-theme"
 AUTH="-i /Users/svetzal/.ssh/twm-aws.pem"
 
-scp -r $AUTH * $TARGET:$LOCATION
-ssh $AUTH $TARGET "chown -R xpressions:psacln $LOCATION"
-ssh $AUTH $TARGET "rm -f $LOCATION/deploy.sh $LOCATION/push.sh"
+deploy()
+{
+  LOCATION=$1
+
+  scp -r $AUTH * $TARGET:$LOCATION
+  ssh $AUTH $TARGET "chown -R xpressions:psacln $LOCATION"
+  ssh $AUTH $TARGET "rm -f $LOCATION/deploy.sh $LOCATION/push.sh"
+}
+
+echo "Deploying to production..."
+deploy "/var/www/vhosts/xpressions.org/httpdocs/wp-content/themes/xpressions-theme"
+
+echo "Deploying to staging..."
+deploy "/var/www/vhosts/xpressions.org/subdomains/new/httpdocs/wp-content/themes/xpressions-theme"
+
